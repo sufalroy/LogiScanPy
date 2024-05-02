30d184cd0e95f58a68625ace78c28188c4b15fe2
Sufal Roy <sufalroy1997@gmail.com>
2024-05-02 16:11:02 +0530

    Add yolov8 object detection model

--- logiscanpy/core/detector/detector.py
+++ logiscanpy/core/detector/detector.py
@@ -1,7 +1,106 @@
+from typing import List, Tuple
+
 import cv2
 import numpy as np
 import onnxruntime as ort
-from typing import List, Tuple
+
+
+class YOLOv8:
+    """YOLOv8 detection model."""
+
+    def __init__(self, onnx_model: str, confidence_thres: float = 0.5, iou_thres: float = 0.45):
+        """Initialize YOLOv8 object.
+
+        Args:
+            onnx_model (str): Path to the ONNX model file.
+            confidence_thres (float): Confidence threshold for detection.
+            iou_thres (float): Intersection over Union threshold for non-maximum suppression.
+        """
+        self.session = self._build_ort_session(onnx_model)
+        self.confidence_thres = confidence_thres
+        self.iou_thres = iou_thres
+        self.input_height, self.input_width = self.session.get_inputs()[0].shape[2:4]
+
+    @staticmethod
+    def _build_ort_session(onnx_model: str) -> ort.InferenceSession:
+        """Build the ONNX Runtime session.
+
+        Args:
+            onnx_model (str): Path to the ONNX model.
+
+        Returns:
+            ort.InferenceSession: The ONNX Runtime session.
+        """
+        providers = ["CUDAExecutionProvider", "CPUExecutionProvider"] if ort.get_device() == "GPU" \
+            else ["CPUExecutionProvider"]
+        return ort.InferenceSession(onnx_model, providers=providers)
+
+    def preprocess(self, img: np.ndarray) -> np.ndarray:
+        """Preprocess the input image for ONNX model.
+
+        Args:
+            img (np.ndarray): Input image.
+
+        Returns:
+            np.ndarray: Preprocessed image data.
+        """
+        self.img_height, self.img_width = img.shape[:2]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (self.input_width, self.input_height))
+        image_data = np.array(img) / 255.0
+        image_data = np.transpose(image_data, (2, 0, 1))
+        image_data = np.expand_dims(image_data, axis=0).astype(np.float32)
+        return image_data
+
+    def postprocess(self, outputs, target_class_ids=None) -> np.ndarray:
+        """Postprocess the output of the ONNX model.
+
+        Args:
+            outputs (np.ndarray): Output from the ONNX model.
+            target_class_ids (List[int], optional): List of class IDs to detect. If None, all classes are detected.
+
+        Returns:
+            np.ndarray: Array of detections in the format [x1, y1, x2, y2, score, class_id].
+        """
+        outputs = np.transpose(np.squeeze(outputs))
+        x_factor = self.img_width / self.input_width
+        y_factor = self.img_height / self.input_height
+
+        mask = np.amax(outputs[:, 4:], axis=1) >= self.confidence_thres
+
+        if target_class_ids is not None:
+            class_ids = np.argmax(outputs[:, 4:], axis=1)
+            mask &= np.isin(class_ids, target_class_ids)
+
+        outputs = outputs[mask]
+
+        boxes = np.concatenate((
+            (outputs[:, :2] - outputs[:, 2:4] / 2) * np.array([x_factor, y_factor]),
+            (outputs[:, :2] + outputs[:, 2:4] / 2) * np.array([x_factor, y_factor])
+        ), axis=1).astype(int)
+        class_ids = np.argmax(outputs[:, 4:], axis=1).astype(np.int32)
+        scores = np.amax(outputs[:, 4:], axis=1)
+
+        indices = cv2.dnn.NMSBoxes(boxes.tolist(), scores.tolist(), self.confidence_thres, self.iou_thres)
+        boxes = boxes[indices]
+        class_ids = class_ids[indices]
+        scores = scores[indices]
+        detections = np.hstack((boxes, scores[:, np.newaxis], class_ids[:, np.newaxis]))
+        return detections
+
+    def detect(self, img, target_class_ids=None) -> np.ndarray:
+        """Detect objects in the input image.
+
+        Args:
+            img (np.ndarray): Input image.
+            target_class_ids (List[int], optional): List of class IDs to detect. If None, all classes are detected.
+
+        Returns:
+            np.ndarray: Array of detections in the format [x1, y1, x2, y2, score, class_id].
+        """
+        img_data = self.preprocess(img)
+        outputs = self.session.run(None, {self.session.get_inputs()[0].name: img_data})
+        return self.postprocess(outputs, target_class_ids)
 
 
 class YOLOv8Seg:
